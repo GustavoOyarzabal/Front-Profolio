7120ff9bad562d12ab951bf87d8eeeb7d6855ab1
GustavoOyarzabal <oyarzabal.gustavo@gmail.com>
2024-06-06 12:17:40 +0200

    cors actulisation 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL + '/api/portfolios';
+const API_URL = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/portfolios`;
 
 const fetchAboutData = async () => {
   const response = await axios.get(`${API_URL}/about`);
@@ -64,68 +64,3 @@ export {
   fetchWorksData,
   fetchFormationData,
 };
-
-// import axios from 'axios'
-
-// const API_URL = 'http://localhost:3001/api/portfolios'
-
-// const fetchAboutData = async () => {
-//   const response = await axios.get(`${API_URL}/about`)
-//   return response.data
-// }
-
-// const fetchNavData = async () => {
-//   const response = await axios.get(`${API_URL}/nav`)
-//   return response.data
-// }
-
-// const fetchServiceData = async () => {
-//   const response = await axios.get(`${API_URL}/service`)
-//   return response.data
-// }
-
-// const fetchFooterData = async () => {
-//   const response = await axios.get(`${API_URL}/footer`)
-//   return response.data
-// }
-
-// const fetchFormData = async () => {
-//   const response = await axios.get(`${API_URL}/form`)
-//   return response.data
-// }
-
-// const fetchHireData = async () => {
-//   const response = await axios.get(`${API_URL}/hire`)
-//   return response.data
-// }
-
-// const fetchPresentationData = async () => {
-//   const response = await axios.get(`${API_URL}/presentation`)
-//   return response.data
-// }
-
-// const fetchExperienceData = async () => {
-//   const response = await axios.get(`${API_URL}/experience`)
-//   return response.data
-// }
-
-// const fetchWorksData = async () => {
-//   const response = await axios.get(`${API_URL}/experience/works`)
-//   return response.data
-// }
-// const fetchFormationData = async () => {
-//   const response = await axios.get(`${API_URL}/formation`)
-//   return response.data
-// }
-// export {
-//   fetchAboutData,
-//   fetchNavData,
-//   fetchServiceData,
-//   fetchFooterData,
-//   fetchFormData,
-//   fetchHireData,
-//   fetchPresentationData,
-//   fetchExperienceData,
-//   fetchWorksData,
-//   fetchFormationData,
-// }
