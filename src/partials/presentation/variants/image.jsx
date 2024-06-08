28c16abac2e04f00027ae2887a934868b5983ff6
GustavoOyarzabal <oyarzabal.gustavo@gmail.com>
2024-06-08 12:27:47 +0200

    video presntation

--- src/partials/presentation/variants/image.jsx
+++ src/partials/presentation/variants/image.jsx
@@ -4,7 +4,7 @@ import { ImageTemplate } from 'root/src/components/section-wrapper'
 const ImageVariant = () => (
   <ImageTemplate
     alt='Hero background'
-    src='https://res.cloudinary.com/dtwacyhiq/image/upload/v1716661323/lzsmkwi1pnw72gymezuy.png'
+    src=''
     width={1920}
     height={1080}
     brightness='32%'
--- src/partials/presentation/variants/video.jsx
+++ src/partials/presentation/variants/video.jsx
@@ -1,19 +1,19 @@
-import { useState, useRef, useEffect } from 'react'
-import { css } from '@emotion/react'
-import videojs from 'video.js'
-import 'video.js/dist/video-js.css'
+import { useState, useRef, useEffect } from 'react';
+import { css } from '@emotion/react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
 const VideoVariant = () => {
-  const videoRef = useRef(null)
-  const playerRef = useRef(null)
-  const [videoLoaded, setVideoLoaded] = useState(false)
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+  const [videoLoaded, setVideoLoaded] = useState(false);
 
   const handlePlayerReady = (player) => {
-    playerRef.current = player
+    playerRef.current = player;
     player.on('canplay', () => {
-      setVideoLoaded(true)
-    })
-  }
+      setVideoLoaded(true);
+    });
+  };
 
   useEffect(() => {
     const options = {
@@ -28,31 +28,31 @@ const VideoVariant = () => {
           type: 'video/mp4',
         },
       ],
-    }
+    };
 
     if (!playerRef.current) {
-      const videoElement = document.createElement('video')
-      videoElement.classList.add('video-js')
-      videoRef.current.appendChild(videoElement)
+      const videoElement = document.createElement('video');
+      videoElement.classList.add('video-js');
+      videoRef.current.appendChild(videoElement);
       playerRef.current = videojs(videoElement, options, () => {
-        if (handlePlayerReady) handlePlayerReady(playerRef.current)
-      })
+        if (handlePlayerReady) handlePlayerReady(playerRef.current);
+      });
     } else {
-      const player = playerRef.current
-      player.autoplay(options.autoplay)
-      player.src(options.sources)
+      const player = playerRef.current;
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
     }
-  }, [videoRef])
+  }, [videoRef]);
 
   useEffect(() => {
-    const player = playerRef.current
+    const player = playerRef.current;
     return () => {
       if (player && !player.isDisposed()) {
-        player.dispose()
-        playerRef.current = null
+        player.dispose();
+        playerRef.current = null;
       }
-    }
-  }, [playerRef])
+    };
+  }, [playerRef]);
 
   return (
     <div
@@ -79,7 +79,93 @@ const VideoVariant = () => {
     >
       <div ref={videoRef} />
     </div>
-  )
-}
+  );
+};
 
-export default VideoVariant
+export default VideoVariant;
+
+// import { useState, useRef, useEffect } from 'react'
+// import { css } from '@emotion/react'
+// import videojs from 'video.js'
+// import 'video.js/dist/video-js.css'
+
+// const VideoVariant = () => {
+//   const videoRef = useRef(null)
+//   const playerRef = useRef(null)
+//   const [videoLoaded, setVideoLoaded] = useState(false)
+
+//   const handlePlayerReady = (player) => {
+//     playerRef.current = player
+//     player.on('canplay', () => {
+//       setVideoLoaded(true)
+//     })
+//   }
+
+//   useEffect(() => {
+//     const options = {
+//       autoplay: true,
+//       muted: true,
+//       preload: 'auto',
+//       controls: false,
+//       loop: true,
+//       sources: [
+//         {
+//           src: 'https://res.cloudinary.com/dtwacyhiq/video/upload/v1716929449/video_ecsvsv.mp4',
+//           type: 'video/mp4',
+//         },
+//       ],
+//     }
+
+//     if (!playerRef.current) {
+//       const videoElement = document.createElement('video')
+//       videoElement.classList.add('video-js')
+//       videoRef.current.appendChild(videoElement)
+//       playerRef.current = videojs(videoElement, options, () => {
+//         if (handlePlayerReady) handlePlayerReady(playerRef.current)
+//       })
+//     } else {
+//       const player = playerRef.current
+//       player.autoplay(options.autoplay)
+//       player.src(options.sources)
+//     }
+//   }, [videoRef])
+
+//   useEffect(() => {
+//     const player = playerRef.current
+//     return () => {
+//       if (player && !player.isDisposed()) {
+//         player.dispose()
+//         playerRef.current = null
+//       }
+//     }
+//   }, [playerRef])
+
+//   return (
+//     <div
+//       css={css`
+//         visibility: ${videoLoaded ? 'visible' : 'hidden'};
+//         video {
+//           object-fit: cover;
+//           height: 100%;
+//           width: 100%;
+//           filter: brightness(40%);
+//           position: absolute;
+//           top: 0;
+//           left: 0;
+//         }
+//         .video-js {
+//           position: absolute;
+//           height: 100%;
+//           width: 100%;
+//           top: 0;
+//           left: 0;
+//         }
+//       `}
+//       data-vjs-player
+//     >
+//       <div ref={videoRef} />
+//     </div>
+//   )
+// }
+
+// export default VideoVariant
