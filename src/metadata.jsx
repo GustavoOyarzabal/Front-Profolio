5911f0e954e3a02bf3eaeb440c6770d1563433b2
GustavoOyarzabal <oyarzabal.gustavo@gmail.com>
2024-06-17 19:15:47 +0200

    tag miniatura 3

--- src/metadata.jsx
+++ src/metadata.jsx
@@ -7,9 +7,9 @@ import Head from 'next/head'
 const Metadata = () => (
   <Head>
     <title>Gustavo Oyarzabal</title>
-    <meta name='description' content='Gustavo Oyarzabal' />
-    <link rel='icon' sizes='32x32' href='/favicon.png' />
-    <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+    <meta name='description' content='Developpeur Web' />
+    <link rel='icon' sizes='32x32' href='https://res.cloudinary.com/dtwacyhiq/image/upload/v1718644385/icono_miniatura_portfolio_pd2ot8.webp' />
+    <link rel='apple-touch-icon' sizes='180x180' href='https://res.cloudinary.com/YOUR_CLOUD_NAME/image/upload/vYOUR_IMAGE_VERSION/icono-miniatura-portfolio.webp' />
     <meta name='viewport' content='width=device-width, initial-scale=1' />
   </Head>
 )
