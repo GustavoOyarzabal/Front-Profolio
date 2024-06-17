96164d24d57b1a703c8dc6bad51a8c34e545733e
GustavoOyarzabal <oyarzabal.gustavo@gmail.com>
2024-06-17 15:39:37 +0200

    tag miniatura 2

--- src/metadata.jsx
+++ src/metadata.jsx
@@ -6,7 +6,7 @@ import Head from 'next/head'
 
 const Metadata = () => (
   <Head>
-    <title>GO</title>
+    <title>Gustavo Oyarzabal</title>
     <meta name='description' content='Gustavo Oyarzabal' />
     <link rel='icon' sizes='32x32' href='/favicon.png' />
     <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
