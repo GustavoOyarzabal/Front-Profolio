6aa843cae88a804b11c2e13d3402b74df2232134
GustavoOyarzabal <oyarzabal.gustavo@gmail.com>
2024-07-26 13:15:50 +0200

    Actualisation en duro experiences 11

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'], // dominio cloudinary
+    domains: ['res.cloudinary.com'],
     formats: ['image/avif', 'image/webp'],
   },
   compiler: {
@@ -15,6 +19,28 @@ const nextConfig = {
     scrollRestoration: true,
     largePageDataBytes: 128 * 100000,
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     domains: ['res.cloudinary.com'], // dominio cloudinary
+//     formats: ['image/avif', 'image/webp'],
+//   },
+//   compiler: {
+//     emotion: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   experimental: {
+//     scrollRestoration: true,
+//     largePageDataBytes: 128 * 100000,
+//   },
+// }
+
+// module.exports = nextConfig
